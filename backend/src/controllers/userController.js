aec83fe989a8a5b8319e97f2d3089561991630c5
RasyaRahmatSyaban <rasyarahmatsyban10@gmail.com>
2025-03-17 19:36:52 +0800

    feat: Add reset password feature for users

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -20,4 +20,16 @@ const update = async (req, res) => {
     }
 }
 
-export default {login, update}; 
+const resetPasswordController = async (req, res) => {
+    try {
+        const { email } = req.body;
+        if (!email) return res.status(400).json({ message: "Email harus diisi!" });
+
+        const response = await service.resetPassword(email);
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export default {login, update,  resetPasswordController}; 
