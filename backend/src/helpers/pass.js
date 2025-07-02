ba4bf89cb103acfceeb6955a6f8ddd080c3b4b5b
rasya_rhmt <rasyarahmatsyban10@gmail.com>
2025-07-02 23:37:25 +0800

    update dependencies

--- backend/src/helpers/pass.js
+++ backend/src/helpers/pass.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const hashPw = async (password) => {
     return await bcrypt.hash(password, 10);
--- backend/src/services/userServices.js
+++ backend/src/services/userServices.js
@@ -1,7 +1,7 @@
 import tokenHelpers from "../helpers/token.js";
 import passHelpers from "../helpers/pass.js";
 import userRepositories from "../repositories/userRepositories.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { UserDTO } from "../dto/dto.js";
 
 const loginAdmin = async (email, password) => {
